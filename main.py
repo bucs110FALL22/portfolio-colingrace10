3823135d13f39f3500bb23f8377f572c9930bd86
colingrace10 <108765291+colingrace10@users.noreply.github.com>
2022-09-06 23:39:54 +0000

    bijcb

---
+++ main.py
@@ -0,0 +1,17 @@
+print(10*5)
+comment:
+print(10**5)
+# 10^5
+print(15/10)
+# 15/10
+print(15//10)
+# 15/10 cuts off denominator no round
+print(-15//5)
+print(15%10)
+#division only focused on remainder 
+print(10%15)
+print(10%10)
+print(0%10)
+print(10/15)
+# repeating decemal 2/3
+
