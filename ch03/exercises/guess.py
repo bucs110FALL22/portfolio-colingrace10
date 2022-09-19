81d9609da116efe2f58427d8dad9aad5bbb78cee
colingrace10 <108765291+colingrace10@users.noreply.github.com>
2022-09-19 21:20:52 +0000

    nfnfnf

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,4 +1,13 @@
 import random
 
-number = range(1, 11)
-corect_number = random.choice(number)
+number = random.randrange(1, 11)
+
+for i in range(3):
+ guess_num = int(input("Pick a number:"))
+ if guess_num > number:
+   print("too high")
+ elif guess_num < number:
+   print("too low")
+ else:
+   print("correct")
+   break 
