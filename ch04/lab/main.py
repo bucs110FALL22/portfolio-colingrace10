c59f0c4b6ff4ecd93617cc5afc41739d9c38f33a
colingrace10 <108765291+colingrace10@users.noreply.github.com>
2022-10-08 03:21:51 +0000

    Lab 4

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -2,7 +2,6 @@ import pygame
 import math
 import random
 import time
-
 # Colors
 pink = (236,117,117)
 blue = (46,135,238)
@@ -15,30 +14,22 @@ green1 = (43,152,15)
 yellow = (247,255,0)
   
 #screen setup part A
-
 pygame.init()
 surface = pygame.display.set_mode((300, 300))
-
 width = pygame.display.get_window_size()[0]
 height = pygame.display.get_window_size()[1]
-
 surface = pygame.display.set_mode((width, height))
 surface.fill(blue)
 pygame.draw.circle(surface, pink, (width/2, height/2), (width/2))
 pygame.draw.line(surface, black, (width/2, 0),(width/2, height))
 pygame.draw.line(surface, black, (0, height/2),(width, height/2))
-
-
 #part B
-
-
 for i in range(10):
  x = random.randrange(0, width)
  y = random.randrange(0, height)
  
  distance_from_center = math.hypot(x-height/2, y-width/2)
  is_in_circle = distance_from_center <= width/2
-
  if is_in_circle: 
    pygame.draw.circle(surface, green, (x, y), (5))
    pygame.display.flip()
@@ -46,22 +37,13 @@ for i in range(10):
    pygame.draw.circle(surface, black, (x, y), (5))
    pygame.display.flip()
   
-
-
 pygame.time.wait(1000)
-
 pygame.display.flip()
-
 pygame.display.update()
-
 surface.fill("blue")
-
 #Part C
-
 surface.fill("green1")
-
 pygame.display.update()
-
 
 pygame.display.update()
 
@@ -79,7 +61,6 @@ print("Blue score", blue_score)
 for i in range(10):
  x = random.randrange(0, width)
  y = random.randrange(0, height)
- 
  distance_from_center = math.hypot(x-height/2, y-width/2)
  is_in_circle = distance_from_center <= width/2
 
@@ -96,13 +77,15 @@ for i in range(10):
    print("Red score:", red_score)
    pygame.display.flip()
    time.sleep(2)
- if is_in_circle: 
-   x = random.randrange(0, width)
-   y = random.randrange(0, height)
- 
-   distance_from_center = math.hypot(x-height/2, y-width/2)
-   is_in_circle = distance_from_center <= width/2
 
+ x = random.randrange(0, width)
+ y = random.randrange(0, height)
+ distance_from_center = math.hypot(x-height/2, y-width/2)
+ is_in_circle = distance_from_center <= width/2
+
+ if is_in_circle: 
+
+   pygame.draw.circle(surface, blue, (x, y), (5))
    pygame.draw.circle(surface, blue1, (x, y), (5))
    blue_score = blue_score + 1 
    print("Blue player hit!")
@@ -118,11 +101,11 @@ for i in range(10):
 
  if red_score > blue_score:
      print("Red Player Wins!!!")
-   elif blue_score > red_score:
+ elif blue_score > red_score:
      print("Blue Player Wins!!!")
-   else:
+ else:
      print("Its a tie!!")
 
-  
+
 pygame.display.update()
 pygame.time.wait(1000)
