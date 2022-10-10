2d5254f200a68fa58e5ba2c822f80a3ca2c76548
colingrace10 <108765291+colingrace10@users.noreply.github.com>
2022-10-10 21:21:19 +0000

    coinflip

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -1,16 +1,32 @@
 import turtle 
 import random
-import pygame
+
 
 window = turtle.Screen() 
 
 colin = turtle.Turtle()
-blue = [0,0,255]
-colin.color(blue)
+#blue = [0,0,255]
+white = [0,0,0]
+colin.color('blue')
+colin.speed(0)
+window.bgcolor(white)
+distance = 10
+angle = 90
+is_in_screen = True
 
-i = rand.range(0,2)
+while is_in_screen:
+ coin = random.randrange(0, 2)
+ if coin == 0:
+  colin.left(angle)
+ else: 
+  colin.right(angle)
+ colin.forward(distance)
 
-while i = 1
-  turtle.left(90)
-  else if i = 0
-  
+ turtlex = colin.xcor
+ turtley = colin.ycor
+
+ xrange = window.window_width()/2
+ yrange = window.window_height()/2
+
+ if abs(turtlex) > xrange or abs(turtley) > yrange:
+   is_in_screen = False
