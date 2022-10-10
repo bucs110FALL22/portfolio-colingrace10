807464ce37d5927cbfcb229a601fd029b2b16c23
colingrace10 <108765291+colingrace10@users.noreply.github.com>
2022-10-10 22:00:42 +0000

    print exercise

---
+++ ch05/exercises/print.py
@@ -0,0 +1,13 @@
+
+
+def star_pyramid():
+
+  rows = int(input("Number of rows:"))
+  stars = int(input("Number of rows:"))
+ 
+
+  for i in range(rows):
+    starsx = stars * "*" 
+    print(starsx)
+
+star_pyramid()
