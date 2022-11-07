f8ea356faaa98f40963ca965fedb672d5227411e
colingrace10 <108765291+colingrace10@users.noreply.github.com>
2022-11-07 21:54:18 +0000

    Lab 7 fr

--- ch07/lab/Surface.py
+++ ch07/lab/Surface.py
@@ -1,9 +1,9 @@
-import Rectangle
+from Rectangle import Rectangle
 
 class Surface:
   # creates the rectangles coordinates and measurements 
   def __init__(self, filename, x, y, h, w):
-   self.rect = Rectangle.rectangle(x, y, h, w)
+   self.rect = Rectangle(x, y, h, w)
    self.image = filename
    #creates and returns the rectangle with corrdiantes
   def getRect(self):
